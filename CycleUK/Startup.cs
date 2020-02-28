765ee333a7f1b44fbdc647456600dcbbbcfaeb92
Mike <michael-wilkins@sky.com>
2020-02-28 13:20:48 +0000

    Initial Commit

---
+++ CycleUK/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CycleUK.Startup))]
+namespace CycleUK
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
