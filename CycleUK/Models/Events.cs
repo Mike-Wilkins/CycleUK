03f52a412b3d600ec205246a18fbfea14dd7a506
Mike <michael-wilkins@sky.com>
2020-03-01 14:26:18 +0000

    Added Events Model, migrations

---
+++ CycleUK/Models/Events.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycleUK.Models
+{
+    public class Events
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage ="Title name is required")]
+        [StringLength(EventsConstants.MAX_TITLE_NAME_LENGTH)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage="Postcode is required")]
+        [StringLength(EventsConstants.MAX_POSTCODE_LENGTH, MinimumLength = EventsConstants.MIN_POSTCODE_LENGTH)]
+        public string Postcode { get; set; }
+
+        [Required(ErrorMessage="Comment is required")]
+        [StringLength(EventsConstants.MAX_COMMENT_LENGTH)]
+        public string Comment { get; set; }
+
+        [Required(ErrorMessage="Date is required")]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+
+
+    }
+}
---
+++ CycleUK/Models/EventsConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycleUK.Models
+{
+    public class EventsConstants
+    {
+        public const int MAX_TITLE_NAME_LENGTH = 50;
+        public const int MAX_POSTCODE_LENGTH = 8;
+        public const int MIN_POSTCODE_LENGTH = 5;
+        public const int MAX_COMMENT_LENGTH = 300;
+
+    }
+}
