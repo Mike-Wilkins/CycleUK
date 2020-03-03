19b52b52520bc676f97b20ce76a0363c22ac67c3
Mike <michael-wilkins@sky.com>
2020-03-03 19:53:05 +0000

    Updated views for Administration Management

--- CycleUK/Controllers/AccountController.cs
+++ CycleUK/Controllers/AccountController.cs
@@ -79,7 +79,9 @@ namespace CycleUK.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    /* return RedirectToLocal(returnUrl);*/
+                    /*return RedirectToAction("Index", "Events");*/
+                    return RedirectToAction("AdminMenu", "Events");
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
@@ -163,7 +165,7 @@ namespace CycleUK.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Events");
                 }
                 AddErrors(result);
             }
@@ -392,7 +394,7 @@ namespace CycleUK.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Events");
         }
 
         //
---
+++ CycleUK/Models/AdminModelView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycleUK.Models
+{
+    public class AdminModelView
+    {
+        public RegisterViewModel loginmodel { get; set; }
+        public Events eventsmodel { get; set; }
+    }
+}
